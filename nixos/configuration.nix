8733bd3058653c54d1f3eb5cd740d0c4b689989d
Chris Jones <chrisjones170187@gmail.com>
2025-05-07 22:09:21 +0100

    enable nvidia drivers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,12 +47,15 @@
   # };
 
   # Enable OpenGL
-  # hardware.graphics = {
-  #  enable = true;
-  #};
+  hardware.graphics = {
+    enable = true;
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+  
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
   
   # Enable GNOME desktop
   services.xserver.displayManager.gdm.enable = true;
@@ -72,40 +75,35 @@
     tali # poker game
     totem # video player
   ]);
-
-  # Load nvidia driver for Xorg and Wayland
-  #services.xserver.videoDrivers = ["nvidia"];
-
-  #hardware.nvidia = {
-
+ 
+  hardware.nvidia = {
     # Modesetting is required.
-   # modesetting.enable = true;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-  #  powerManagement.enable = false;
+    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #  powerManagement.finegrained = false;
+    powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
- #   open = false;
+    open = true;
 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
- #   nvidiaSettings = true;
+    # Enable the Nvidia settings menu, accessible via `nvidia-settings`.
+    nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
- #   package = config.boot.kernelPackages.nvidiaPackages.stable;
- # };
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
   
 
   # Configure keymap in X11
